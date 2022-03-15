c024e3996967e77714d98c4dadcfe2e3afbf1922
seungheeyoon <one4679@naver.com>
2022-03-15 23:29:42 +0900

    2022-03-15

--- march/10.js
+++ march/10.js
@@ -28,3 +28,32 @@ function solution(array, commands) {
     return newArray[position - 1];
   });
 }
+
+// 2. 소수 만들기
+
+function solution(nums) {
+  let answer = 0;
+  const len = nums.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      for (let k = j + 1; k < len; k++) {
+        let number = nums[i] + nums[j] + nums[k];
+
+        if (isPrime(number)) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  function isPrime(sum) {
+    if (sum < 2) return true;
+    for (let i = 2; i < sum; i++) {
+      if (sum % i == 0) return false;
+    }
+    return true;
+  }
+
+  return answer;
+}
